9cc9901d84acb966fe7f6a4abe7707633e0c58fb
KhajonpopC <70530196+KhajonpopC@users.noreply.github.com>
2020-09-13 23:47:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var calc = document.body.querySelector(".calculator");
+
+var firstnumber = prompt("Enter a number");
+
+if(isNaN(firstnumber)==true){
+ calc.innerHTML="A valid number was not inputed";
+}
+else if(false){
+    var operation = prompt("Enter a operation sign");
+}
+   if 
+
+ if("operation sign"==operation){
+  var secondnumber = prompt("Enter a second number");
+}
+else{
+  calc.innerHTML="A valid operation was not inputted";
+}
+
+var calculation = firstnumber operation secondnumber
+// isNaN
